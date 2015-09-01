583fd10cb4fdd54e3449e7c5d0c5814a871064f5
nhelvig <nhelvig@berkeley.edu>
2015-09-01 16:55:43 -0700

    Deposit test passes.

---
+++ src/main/java/com/thoughtworks/tddintro/accountbalance/Account.java
@@ -0,0 +1,20 @@
+package com.thoughtworks.tddintro.accountbalance;
+
+/**
+ * Created by nhelvig on 9/1/15.
+ */
+public class Account {
+    private int balance;
+
+    public Account(int initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public void deposit(int amount) {
+        this.balance += amount;
+    }
+
+    public int getBalance() {
+        return this.balance;
+    }
+}
